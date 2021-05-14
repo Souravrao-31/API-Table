00c17d7613bad68e0ab393f268f935f5ebba8428
Sourav <souravrao3110@gmail.com>
2021-05-14 22:28:51 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 function App() {
+   
+   const [product, setProduct] = useState([])
+   const [search, setsearch] = useState()
+
+   const getProductdata =async () =>{
+
+    try{
+       const data = await axios.get('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline');
+       console.log(data.data);
+      setProduct(data.data);
+    }
+    catch(e){
+     console.log(e)
+    }
+
+  };
+
+  useEffect( () =>{
+    getProductdata();
+  }
+  ,[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <h1>Table</h1>
+
+   <input
+    type = "text"
+    placeholder = "Search here"
+    onChange= { (e) => {
+      setsearch(e.target.value)
+    }}
+   />
+   
+    {
+      product.filter(item => {
+        if(search == ""){
+            return item;
+        }
+        else if(item.name.toLowerCase().includes(search.toLowerCase()) ){
+              return item;
+        }
+      })
+      .map(item => {
+          return 
+         (
+            <p> 
+            {item.name} 
+            </p>
+         );
+      })
+    }
+
+
     </div>
   );
 }
